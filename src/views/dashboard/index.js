4bb5093be55c3882d247713401c6ef9178f96ec5
karis <karismmm@gmail.com>
2019-11-01 13:59:40 +0900

    배포테스트

--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -4,7 +4,7 @@ import { MDBContainer, MDBIframe } from "mdbreact";
 const IframePage = () => {
     return (
         <MDBContainer className="text-center">
-            <MDBIframe src="http://15.164.111.122:5601/app/kibana#/dashboard/b90faa00-e78c-11e9-8acc-f3cddb6dfb41?embed=true&_g=(refreshInterval%3A(pause%3A!f%2Cvalue%3A5000)%2Ctime%3A(from%3Anow-30d%2Cto%3Anow))" height="1250px" />
+            <MDBIframe src="https://db.argoport.com:5601/app/kibana#/dashboard/b90faa00-e78c-11e9-8acc-f3cddb6dfb41?embed=true&_g=(refreshInterval%3A(pause%3A!f%2Cvalue%3A5000)%2Ctime%3A(from%3Anow-30d%2Cto%3Anow))" height="1250px" />
         </MDBContainer>
     );
 }
