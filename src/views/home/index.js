e4fea70ee7676ed726eb3f55086300dda87e3231
ags0688 <ags0688@gmail.com>
2019-11-16 13:17:34 +0900

    index 화면 수정

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -48,58 +48,58 @@ class Home extends React.Component {
             <Aux>
                 <Breadcrumb/>
                 <div>
-                    <div class="simpleslide100">
-                        <div class="simpleslide100-item bg-img1" style={sectionStyle1}/>
-                        <div class="simpleslide100-item bg-img1" style={sectionStyle2}/>
+                    <div className="simpleslide100">
+                        <div className="simpleslide100-item bg-img1" style={sectionStyle1}/>
+                        <div className="simpleslide100-item bg-img1" style={sectionStyle2}/>
                     </div>
 
-                    <div class="flex-col-c-sb size1 overlay1">
-                        <div class="w-full flex-w flex-sb-m p-l-80 p-r-80 p-t-22 p-lr-15-sm">
-                            <div class="wrappic1 m-r-30 m-t-10 m-b-10">
+                    <div className="flex-col-c-sb size1 overlay1">
+                        <div className="w-full flex-w flex-sb-m p-l-80 p-r-80 p-t-22 p-lr-15-sm">
+                            <div className="wrappic1 m-r-30 m-t-10 m-b-10">
                             </div>
 
-                            <div class="flex-w m-t-10 m-b-10">
-                                <a href="#" class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
-                                    <i class="feather icon-smartphone auth-icon"/>
+                            <div className="flex-w m-t-10 m-b-10">
+                                <a href="#" className="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
+                                    <i className="feather icon-smartphone auth-icon"/>
                                 </a>
                                 &nbsp;
-                                <NavLink to="/auth/signup" class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
-                                    <i class="feather icon-user-plus auth-icon"/>
+                                <NavLink to="/auth/signup" className="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
+                                    <i className="feather icon-user-plus auth-icon"/>
                                 </NavLink>
                                 &nbsp;
-                                <NavLink to="/auth/signin" class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
-                                    <i class="feather icon-unlock auth-icon"/>
+                                <NavLink to="/auth/signin" className="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
+                                    <i className="feather icon-unlock auth-icon"/>
                                 </NavLink>
                             </div>
                         </div>
 
-                        <div class="flex-col-c-m p-l-15 p-r-15 p-t-50 p-b-120">
-                            <h3 class="l1-txt1 txt-center p-b-40 respon1">
+                        <div className="flex-col-c-m p-l-15 p-r-15 p-t-50 p-b-120">
+                            <h3 className="l1-txt1 txt-center p-b-40 respon1">
                                 Argo is Coming Soon
                             </h3>
 
-                            <div class="flex-w flex-c-m cd100">
-                                <div class="flex-col-c wsize1 m-b-30">
-                                    <span class="l1-txt2 p-b-9 year">2020Y</span>
+                            <div className="flex-w flex-c-m cd100">
+                                <div className="flex-col-c wsize1 m-b-30">
+                                    <span className="l1-txt2 p-b-9 year">2020Y</span>
                                 </div>
 
-                                <div class="flex-col-c wsize1 m-b-30">
-                                    <span class="l1-txt2 p-b-9 quarter">1Q</span>
+                                <div className="flex-col-c wsize1 m-b-30">
+                                    <span className="l1-txt2 p-b-9 quarter">1Q</span>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="flex-w flex-c-m p-b-35">
-                            <a href="#" class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
-                                <i class="fa fa-facebook"/>
+                        <div className="flex-w flex-c-m p-b-35">
+                            <a href="#" className="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
+                                <i className="fa fa-facebook"/>
                             </a>
 
-                            <a href="#" class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
-                                <i class="fa fa-twitter"/>
+                            <a href="#" className="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
+                                <i className="fa fa-twitter"/>
                             </a>
 
-                            <a href="#" class="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
-                                <i class="fa fa-youtube-play"/>
+                            <a href="#" className="size3 flex-c-m how-social trans-04 m-r-3 m-l-3 m-b-5">
+                                <i className="fa fa-youtube-play"/>
                             </a>
                         </div>
                     </div>
